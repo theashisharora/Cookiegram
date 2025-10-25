06a9ff47b90f2ecee795a9ff8e9dbba1c873f458
Ashish Arora <arashish@sheridancollege.com>
2025-10-25 10:01:20 -0400

    chore: initial commit

---
+++ src/main/java/com/cookiegram/app/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.cookiegram.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+    public String landing() {
+        return "landing";
+    }
+}
---
+++ src/test/java/com/cookiegram/app/CookiegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookiegram.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookiegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
